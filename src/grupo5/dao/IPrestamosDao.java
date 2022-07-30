e16e0c1e15fab26e86a70785c369af3f275e6a3a
JasonProg666 <108161014+JasonProg666@users.noreply.github.com>
2022-07-29 22:25:59 -0300

    Baja de cliente
    Hice la baja agregando el campo "estado" a la tabla de clientes, si esta en 1 esta activo, si esta en 0 esta inactivo. 
    Se verifica que el cliente no tenga prestamos asignados antes de inactivar

--- src/grupo5/dao/IPrestamosDao.java
+++ src/grupo5/dao/IPrestamosDao.java
@@ -1,5 +1,11 @@
 package grupo5.dao;
 
+import java.util.List;
+
+import grupo5.entidad.Prestamos;
+
 public interface IPrestamosDao {
+
+	public List<Prestamos> ObtenerFiltros(Integer Id, Integer IdCliente);
 
 }
