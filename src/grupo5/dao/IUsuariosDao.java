5e0608a653808428d8984d4b08eeece3d05474e0
JasonProg666 <108161014+JasonProg666@users.noreply.github.com>
2022-08-01 15:56:28 -0300

    Usuarios
    Funcionalidad de usuarios.
    Agrego Metodos a la interfaz

--- src/grupo5/dao/IUsuariosDao.java
+++ src/grupo5/dao/IUsuariosDao.java
@@ -1,5 +1,11 @@
 package grupo5.dao;
 
+import grupo5.entidad.Usuarios;
+
 public interface IUsuariosDao {
+
+	Boolean Verificar(String Usuario, String Pass);
+
+	Boolean CargarTablaDefault(Usuarios user);
 
 }
