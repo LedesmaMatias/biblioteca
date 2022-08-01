11683c888ee7555630240bbe8fcf698444f71b8d
JasonProg666 <108161014+JasonProg666@users.noreply.github.com>
2022-08-01 15:59:37 -0300

    Usuarios
    Funcionalidad de usuarios

--- src/grupo5/negocio/INegUsuarios.java
+++ src/grupo5/negocio/INegUsuarios.java
@@ -1,5 +1,11 @@
 package grupo5.negocio;
 
+import grupo5.entidad.Result;
+
 public interface INegUsuarios {
+
+	Boolean VerificarUsuarios(String Usuario, String Pass);
+
+	Result CargarTablaDefault();
 
 }
