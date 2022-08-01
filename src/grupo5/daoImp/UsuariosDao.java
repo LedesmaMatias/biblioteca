fe9a1ee5323f8eba0d47f3f97fd52d810da0a364
JasonProg666 <108161014+JasonProg666@users.noreply.github.com>
2022-08-01 15:57:39 -0300

    usuarios
    Funcionalidad de usuarios.
    Agrego Metodos

--- src/grupo5/daoImp/UsuariosDao.java
+++ src/grupo5/daoImp/UsuariosDao.java
@@ -1,9 +1,14 @@
 package grupo5.daoImp;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import grupo5.dao.IUsuariosDao;
+import grupo5.entidad.Usuarios;
 
 @Repository("UsuariosDao")
 public class UsuariosDao implements IUsuariosDao {
@@ -11,4 +16,47 @@ public class UsuariosDao implements IUsuariosDao {
 	@Autowired
 	private Conexion conexion;
 
+	@Override
+	public Boolean CargarTablaDefault(Usuarios user) {
+
+		Session session = conexion.abrirConexion(); // Abro conexion
+		Transaction tx = session.beginTransaction(); // Empiezo Transaccion
+
+		boolean aux = true;
+
+		try {
+
+			session.save(user); // Guardo en base
+
+		} catch (Exception e) {
+			aux = false;// por si algo sale mal
+			tx.rollback();
+		}
+
+		tx = session.getTransaction(); // Commit de la trans
+		tx.commit();
+		conexion.cerrarSession();
+
+		return aux;
+	}
+
+	@Override
+	public Boolean Verificar(String Usuario, String Pass) {
+
+		Boolean hayRegistros = false;
+		Session session = conexion.abrirConexion(); // Abro conexion
+
+		List<Object[]> rl = session
+				.createQuery(
+						"select u from Usuarios u where " + " (user = '" + Usuario + "') and (pass = '" + Pass + "')")
+				.list();
+
+		if (rl.size() != 0) {
+			hayRegistros = true;
+		}
+
+		conexion.cerrarSession();
+
+		return hayRegistros;
+	}
 }
